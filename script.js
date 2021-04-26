b4997621e9813d389d01640e0ad58e2ae062c2c8
Anthony McCoy <james.anthony.mccoy@gmail.com>
2021-04-25 22:01:55 -0500

    App is functional and done. Could get get strike thru working

---
+++ script.js
@@ -0,0 +1,56 @@
+// Show an unordered list of to do's
+// Show an input to enter a new to do
+// Show a button to add a to  do. When the button is clicked:
+// The text from the input box is used to add a list item to the bottom of the list
+// The text from the input box is cleared out.
+// When the user clicks on a list item, it is removed
+
+// Extra Credit: - When a list item is clicked, cross it out, then remove it after 1 second.
+
+// declaring global variables for grabbing elements in HTML
+const wholeList = document.querySelector("#wholeList");
+const submit = document.querySelector("#submit");
+const input = document.querySelector("#input");
+
+// event listener for clicking on 'Add to List' on page
+submit.addEventListener("click", addItem);
+
+// add item to list function
+function addItem(event) {
+  event.preventDefault();
+  let newItem = document.getElementById("input").value;
+  let newLi = document.createElement("li");
+  // alerts user if submit button is clicked with no text in box
+  if (!newItem) {
+    alert("Add a task in the submission box!");
+  } else {
+    newLi.innerHTML = newItem;
+    wholeList.appendChild(newLi);
+    document.getElementById("input").value = "";
+  }
+}
+
+// remove list item if clicked. has 1s delay
+wholeList.addEventListener("click", removeItem);
+
+function removeItem(e) {
+  e.target.remove();
+}
+
+// CANNOT GET WORKING - STRIKE THRU
+
+// function removeItem(e) {
+//     function lineStrike(){
+//         wholeList.innerHTML = textDecoration.line-through;
+//     }
+//   setTimeout(function(){e.target.remove();
+// }, 1000)};
+
+
+// CANNOT GET WORKING - LIVE UPDATE LIST OF ITEMS REMAINING IN LIST
+// const h3 = document.querySelector("#remaining");
+// let itemAmount = document.querySelector("ul").children;
+// let remaining = `Total number of to do tasks remaining: ${itemAmount.length}`
+// let numberLeft = document.createElement("h4")
+// numberLeft.innerHTML = remaining;
+// h3.appendChild(numberLeft);
